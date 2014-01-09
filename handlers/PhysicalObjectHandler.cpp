7acc864703f673c270f4ef93f6e883a4a1889aac
Damjan Miklic <damjan.miklic@gmail.com>
2014-01-09 22:06:34 +0100

    Updated messages from upstream. Added PhysicalObjectHandler files (still not operational).

---
+++ handlers/PhysicalObjectHandler.cpp
@@ -0,0 +1,122 @@
+/*
+
+ */
+#include <iostream>
+
+#include <boost/foreach.hpp>
+
+#include <zmq.hpp>
+#include "playground/zmq_helpers.hpp"
+
+#include "PhysicalEngine.h"
+#include "handlers/PhysicalObjectHandler.h"
+
+// Protobuf message headers
+#include "base_msgs.pb.h"
+#include "dev_msgs.pb.h"
+#include "sim_msgs.pb.h"
+
+using zmq::message_t;
+using zmq::socket_t;
+using std::string;
+using std::cerr;
+using std::endl;
+
+using namespace AssisiMsg;
+
+namespace Enki
+{
+
+// -----------------------------------------------------------------------------
+
+    /* virtual */
+    string PhysicalObjectHandler::createObject(const std::string& data, 
+                                     World* world)
+    {
+        string name = "";
+        Spawn spawn_msg;     
+        assert(spawn_msg.ParseFromString(data));
+        if (objects_.count(spawn_msg.name()) < 1)
+        {
+            name = spawn_msg.name();
+            Point pos(spawn_msg.pose().position().x(),
+                      spawn_msg.pose().position().y());
+            double yaw(spawn_msg.pose().orientation().z());
+            casus_[name] = new Casu;
+            casus_[name]->pos = pos;
+            casus_[name]->angle = yaw;
+            world->addObject(casus_[name]);
+        }
+        else
+        {
+            cerr << "Casu "<< spawn_msg.name() << " already exists." << endl;
+        }
+        return name;
+    }
+
+// -----------------------------------------------------------------------------
+
+    /* virtual */
+    int PhysicalObjectHandler::handleIncoming(const std::string& name,
+                                    const std::string& device,
+                                    const std::string& command,
+                                    const std::string& data)
+    {
+        int count = 0;
+        if (device == "DiagnosticLed")
+        {
+            if (command == "On")
+            {
+                ColorStamped color_msg;
+                assert(color_msg.ParseFromString(data));
+                casus_[name]->top_led.on( Enki::Color(color_msg.color().red(),
+                                                      color_msg.color().green(),
+                                                      color_msg.color().blue(),
+                                                      color_msg.color().alpha() ) );
+                count++;
+            }
+            else if (command == "Off")
+            {
+                casus_[name]->top_led.off( );
+                count++;
+            }
+            else
+            {
+                cerr << "Unknown command for " << name << "/" << device << endl;
+                return 0;
+            }         
+        }
+        else
+        {
+            cerr << "Unknown device " << device << endl;
+        }
+        return count;
+    }
+
+// -----------------------------------------------------------------------------
+
+    /* virtual */
+    int PhysicalObjectHandler::sendOutgoing(socket_t& socket)
+    {
+        int count = 0;
+        BOOST_FOREACH(const CasuMap::value_type& ca, casus_)
+        {
+            std::string data;
+            /* Publishing IR readings */
+            RangeArray ranges;
+            BOOST_FOREACH(IRSensor* ir, ca.second->range_sensors)
+            {
+                ranges.add_range(0.01*ir->getDist());                
+            }
+            ranges.SerializeToString(&data);
+            zmq::send_multipart(socket, ca.first, "ir", "ranges", data);
+
+            /* Publish other stuff as necessary ... */
+
+            count++;
+        }
+        return count;
+    }
+// -----------------------------------------------------------------------------
+
+}
---
+++ handlers/PhysicalObjectHandler.h
@@ -0,0 +1,64 @@
+/*! \file  PhysicalObjectHandler.h
+    \brief Implementation of the generic object handler interface.
+    
+    Handles physical objects.
+
+    TODO: Specfic robot handlers should probably inherit from this class,
+          for generic object-handling capabilities.
+*/
+
+#ifndef ENKI_PHYSICAL_OBJECT_HANDLER_H
+#define ENKI_PHYSICAL_OBJECT_HANDLER_H
+
+#include <map>
+
+#include "handlers/ObjectHandler.h"
+
+namespace Enki
+{
+
+    class PhysicalObject;
+    class World;
+
+    //! Handling of PhysicalObjects
+    /*!
+
+     */
+    class PhysicalObjectHandler : public ObjectHandler
+    {
+    public:
+        PhysicalObjectHandler() { }
+        virtual ~PhysicalObjectHandler() { }
+
+        //! Casu factory method
+        /*! Creates PhysicalObjects.
+
+          Keeps a pointer to the created object, but does not
+          delete it in the destructor.
+
+          \return Returns the name of the created object.
+        */
+        virtual std::string createObject(const std::string& data, World* world);
+
+        //! Handle incoming message
+        /*! Handles casu actuator commands.
+
+         */
+        virtual int handleIncoming(const std::string& name,
+                                   const std::string& device,
+                                   const std::string& command,
+                                   const std::string& data);
+
+        //! Assemble outgoing messages.
+        /*! Sends CASU sensor data messages.
+
+         */
+        virtual int sendOutgoing(zmq::socket_t& socket);
+
+    private:
+        typedef std::map<std::string, PhysicalObject*> PhysicalObjectMap;
+        PhysicalObject objects_;
+    };
+}
+
+#endif
