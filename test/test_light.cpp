05bbe0c6bcb1e438b59864a93ccd8836ac9dc336
Pedro Mariano <plmariano@fc.ul.pt>
2014-01-17 15:55:46 +0000

    Added a function to write a light intensity map.

--- test/test_light.cpp
+++ test/test_light.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 #include <enki/PhysicalEngine.h>
 
@@ -16,10 +17,10 @@ public:
 	Test (Enki::Vector location, Enki::World *world, double wavelength)
 	{
 		this->pos = location;
-		this->lightSensor = new LightSensor (10, this, Enki::Vector (0, 1), 0, wavelength);
+		this->lightSensor = new LightSensor (20, this, Enki::Vector (0, 1), 0, wavelength);
 		this->lightSource = new LightSourceFromAbove
-			(10.0, this, Enki::Vector (0, -1), 0.0,
-			 500.0,   // k
+			(20.0, this, Enki::Vector (0, -1), 0.0,
+			 1.0,   // k
 			 9.0,     // radius
 			 wavelength,
 			 100.0,   // max intensity
@@ -30,40 +31,68 @@ public:
 
 		world->addObject (this->lightSource);
 		this->addLocalInteraction (this->lightSensor);
+		PhysicalObject::dryFrictionCoefficient = 1000; // Casus are immovable
 
 		// this->setCylindric (CASU::RADIUS, CASU::HEIGHT, CASU::MASS);
 		this->setCylindric (0, 0, -1);
 	}
 };
 
-Test *test1;
-Test *test2;
+Test *test[2];
+
 
 void debug (int time);
+void sampleLightIntensity (Enki::World *world, unsigned int numberSamples, double wavelength);
 
 int main (int argc, char *argv[])
 {
 	// Create the world
-	Enki::World world (200, 200);
+	Enki::World world (50, 50);
 
-	test1 = new Test (Vector (-4, 0), &world, 450);
-	test2 = new Test (Vector (4, 0), &world, 550);
+	test[0] = new Test (Vector (10, 10), &world, 450);
+	test[1] = new Test (Vector (15, 10), &world, 550);
 
-	world.addObject (test1);
-	world.addObject (test2);
+	world.addObject (test[0]);
+	world.addObject (test[1]);
 	debug (-1);
 	for (int it = 0; it < 3; it++) {
 		world.step (0.05);
 		debug (it);
 	}
+	sampleLightIntensity (&world, 50, 450);
 	return 0;
+}
+
+void sampleLightIntensity (Enki::World *world, unsigned int numberSamples, double wavelength)
+{
+	const double deltaTime = 1;
+	double dx = world->w / numberSamples;
+	double dy = world->h / numberSamples;
+	double deltaSpace = std::min (dx, dy);
+	Point position;
+	for (position.x = 0; position.x < world->w; position.x += deltaSpace) {
+		for (position.y = 0; position.y < world->h; position.y += deltaSpace) {
+			double intensity = 0;
+			for (World::ObjectsIterator object = world->objects.begin (); object != world->objects.end (); ++object) {
+				LightSource *lightSource = dynamic_cast<LightSource *>(*object);
+				if (lightSource != NULL) {
+					intensity += lightSource->getIntensityAt (position, wavelength);
+				}
+			}
+			std::cout << '\t' << intensity;
+		}
+		std::cout << '\n';
+	}
 }
 
 void debug (int time)
 {
 	std::cout << time;
-	std::cout << '\t' << test1->lightSensor->getIntensity ();
-	std::cout << '\t' << test1->pos;
-	std::cout << '\t' << test2->lightSensor->getIntensity ();
+	for (int i = 0; i < 2; i++) {
+		std::cout << '\t' << test [i]->pos;
+		std::cout << '\t' << test [i]->lightSensor->getIntensity ();
+		std::cout << '\t' << test [i]->lightSource->getAbsolutePosition ();
+	}
+
 	std::cout << '\n';
 }
