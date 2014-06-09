1a58e6924efdba89a1836494137f9c288db44701
Pedro Mariano <plmariano@fc.ul.pt>
2014-06-09 18:18:30 +0100

    Visualization code.

--- playground/AssisiPlayground.h
+++ playground/AssisiPlayground.h
@@ -27,6 +27,43 @@ class AssisiPlayground : public ViewerWidget
 {
 
     Q_OBJECT;
+		/**
+		 * Data layers that can be drawn in the window.
+		 */
+		enum Layer {NONE, HEAT, ELECTRIC_FIELD, VIBRATION, LIGHT};
+		/**
+		 * Heat physic simulation used in the underlying world.
+		 */
+		const WorldHeat *worldHeat;
+		/**
+		 * Maximum presentable heat.  Ambient heat is drawn black, higher
+		 * values are drawn in shades of red while lower values are drawn in
+		 * shades of blue.
+		 */
+		const double maxHeat;
+		/**
+		 * The layer to drawn upon robots. 
+		 */
+		Layer layerToDraw;
+		/**
+		 * Transparency of the layer drawn above robots.
+		 */
+		double transparency;
+		/**
+		 * Whether to draw data using a gradient or not.
+		 */
+		bool useGradient;
+
+		Point dataSize;
+		std::vector<std::vector<std::vector<float> > > dataColour;
+		/**
+		 * Whether to show a help message or not.
+		 */
+		bool showHelp;
+		/**
+		 * The world that is shown in this widget.
+		 */
+		ExtendedWorld *extendedWorld;
 
 public:
     AssisiPlayground(World *world, QWidget *parent = 0);	
