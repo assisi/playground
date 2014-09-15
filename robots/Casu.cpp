2de970c8cee434ed71d42ae6524d10e92656d534
Pedro Mariano <mariano.pedro@gmail.com>
2014-09-15 14:51:55 +0100

    Temperature relative position.

--- robots/Casu.cpp
+++ robots/Casu.cpp
@@ -34,10 +34,10 @@ namespace Enki
     const double Casu::TEMP_SENS_COUNT = 5;
     const double Casu::MIN_MEASURABLE_HEAT = 0.0;
     const double Casu::MAX_MEASURABLE_HEAT = 100.0;
-    const Vector Casu::TEMP_SENS_NORTH_POS = (2.5,0.0);
-    const Vector Casu::TEMP_SENS_EAST_POS = (0.0,-2.5);
-    const Vector Casu::TEMP_SENS_SOUTH_POS = (-2.5,0.0);
-    const Vector Casu::TEMP_SENS_WEST_POS = (0.0,2.5);
+    const Vector Casu::TEMP_SENS_NORTH_POS = Vector (2.5,0.0);
+    const Vector Casu::TEMP_SENS_EAST_POS = Vector (0.0,-2.5);
+    const Vector Casu::TEMP_SENS_SOUTH_POS = Vector (-2.5,0.0);
+    const Vector Casu::TEMP_SENS_WEST_POS = Vector (0.0,2.5);
     const Vector Casu::TEMP_SENS_CENTER_POS = Vector(0.0,0.0);
 
 Casu::Casu(World* world) :
